5b8b77f9f0f173db105f29380b8fea1109b95426
ychong032 <yowlimchong2403@gmail.com>
2023-07-31 17:30:26 +0800

    Add middleware

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,8 @@ mongoose
 app.set("view engine", "ejs");
 
 // Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
