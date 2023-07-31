83371ee99643987c4361d0d62b11c87e68840752
ychong032 <yowlimchong2403@gmail.com>
2023-07-31 15:23:42 +0800

    Connect to database using environment variables

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
+require("dotenv").config({ path: __dirname + "/../.env" });
 const express = require("express");
 const morgan = require("morgan");
-const mongoose = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// Connect to database
+mongoose
+	.connect(process.env["DB_URI"])
+	.then((result) => app.listen(process.env["PORT_NUMBER"]))
+	.catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
+	console.log("Connected to database!");
 	res.send("Hello world!");
 });
-
-app.listen(3000);
