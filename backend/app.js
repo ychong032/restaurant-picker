e75e4d1b8d8a8e5f1ff8d0a53d2be9eaaa8747d2
ychong032 <yowlimchong2403@gmail.com>
2023-08-04 16:09:48 +0800

    Clean up unused code and package

--- backend/app.js
+++ backend/app.js
@@ -17,17 +17,10 @@ mongoose
 	})
 	.catch((err) => console.log(err));
 
-// Set view engine
-app.set("view engine", "ejs");
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-	res.send("Hello world!");
-});
 
 app.use("/api/restaurants", restaurantRouter);
