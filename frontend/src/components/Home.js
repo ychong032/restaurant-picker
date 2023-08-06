3dbbac898b9cd8cf90141735eff0b1a68d1f1adb
ychong032 <yowlimchong2403@gmail.com>
2023-08-06 21:04:43 +0800

    Implement ability to get another restaurant

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,17 +1,31 @@
 import Restaurant from "./Restaurant";
+import { useEffect, useState } from "react";
 
 const Home = ({ restaurants }) => {
-	const getRandomRestaurant = () =>
-		restaurants[Math.floor(Math.random() * restaurants.length)];
+	const [randomRestaurant, setRandomRestaurant] = useState({});
+
+	useEffect(() => {
+		setRandomRestaurant(
+			restaurants[Math.floor(Math.random() * restaurants.length)]
+		);
+	}, [restaurants]);
+
+	const handleClick = () => {
+		setRandomRestaurant(
+			restaurants[Math.floor(Math.random() * restaurants.length)]
+		);
+	};
 
 	return (
 		<div className="bg-gray-200 flex flex-col items-center w-fit p-4 rounded-md gap-4">
-			{restaurants.length ? (
-				<Restaurant restaurant={getRandomRestaurant()} />
+			{randomRestaurant ? (
+				<Restaurant restaurant={randomRestaurant} />
 			) : (
 				<p>No restaurants in database.</p>
 			)}
-			<button className="p-4 rounded-xl bg-sky-200 hover:bg-sky-300 active:bg-sky-400">
+			<button
+				className="p-4 rounded-xl bg-sky-200 hover:bg-sky-300 active:bg-sky-400"
+				onClick={handleClick}>
 				Get another restaurant
 			</button>
 		</div>
