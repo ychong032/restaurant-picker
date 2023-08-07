5d390311e4e20fbb45f8261a9a69be12c98cc26d
ychong032 <yowlimchong2403@gmail.com>
2023-08-07 17:34:07 +0800

    Modify styling of Success page

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const Success = () => {
 	return (
-		<div className="bg-gray-200 p-4 text-center">
+		<div className="bg-gray-200 p-6 text-center rounded-md">
 			<p className="mb-4">Successfully added a new restaurant!</p>
 			<Link
-				className="border-2 bg-blue-300 p-2 hover:bg-blue-400 active:bg-blue-500"
+				className="border-2 bg-blue-300 p-2 hover:bg-blue-400 active:bg-blue-500 rounded-md"
 				to="/restaurants"
 				replace="true">
 				Back to restaurants
