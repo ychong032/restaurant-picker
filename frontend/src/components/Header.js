e34d34a6378cfb01bf64c833598c6e726f171014
ychong032 <yowlimchong2403@gmail.com>
2023-08-14 16:08:10 +0800

    Modify header styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,10 @@ const Header = () => {
 	return (
 		<div className="flex justify-between items-center px-60 py-4 bg-sky-300">
 			<NavLink to="/">
-				<h1 className="text-2xl font-bold">Restaurant Picker</h1>
+				<div className="flex gap-2 items-center">
+					<i className="fa-solid fa-bowl-rice fa-2xl"></i>
+					<h1 className="text-2xl font-bold">Restaurant Picker</h1>
+				</div>
 			</NavLink>
 			<nav>
 				<ul className="flex items-center gap-8">
@@ -13,8 +16,8 @@ const Header = () => {
 							to="/"
 							className={({ isActive }) =>
 								isActive
-									? "border-b-4 border-blue-700 text-blue-700 "
-									: "hover:text-blue-500"
+									? "border-b-4 border-blue-700 text-blue-700 font-semibold"
+									: "hover:text-blue-500 font-semibold"
 							}>
 							Pick a restaurant
 						</NavLink>
@@ -24,8 +27,8 @@ const Header = () => {
 							to="/restaurants"
 							className={({ isActive }) =>
 								isActive
-									? "border-b-4 border-blue-700 text-blue-700 "
-									: "hover:text-blue-500"
+									? "border-b-4 border-blue-700 text-blue-700 font-semibold"
+									: "hover:text-blue-500 font-semibold"
 							}>
 							All restaurants
 						</NavLink>
@@ -35,8 +38,8 @@ const Header = () => {
 							to="/create"
 							className={({ isActive }) =>
 								isActive
-									? "border-b-4 border-blue-700 text-blue-700"
-									: "hover:text-blue-500"
+									? "border-b-4 border-blue-700 text-blue-700 font-semibold"
+									: "hover:text-blue-500 font-semibold"
 							}>
 							New restaurant
 						</NavLink>
